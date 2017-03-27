a2a51f98702c83ec829c77d099a4e5a81bdcb56b
Matthias Leutenegger <mleutenegger@syntro.ch>
2017-03-27 13:16:51 +0200

    init2

--- spherpro/datastore.py
+++ spherpro/datastore.py
@@ -1,6 +1,25 @@
+import pandas as pd
+import numpy as np
+
 
 class DataStore(object):
     """docstring for DataStore."""
-    def __init__(self, arg):
+    def __init__(self):
         super(DataStore, self).__init__()
-        print(arg)
+        # init empty properties here
+        ExperimentLayout = None
+        BarcodeKey = None
+        WellMeasurements = None
+        CutMeta = None
+        RoiMeta = None
+        ChannelMeta = None
+        SphereMeta = None
+        MeasurementData = None
+
+    def readConfig(self, configpath):
+        # Read the config and save the properties
+        raise NotImplementedError
+
+    def readData(self):
+        # Read the data based on the config
+        raise NotImplementedError
