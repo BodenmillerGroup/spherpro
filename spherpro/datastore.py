7da417a0f4ddcce948c68e377c6ef475cf6d9002
Matthias Leutenegger <mleutenegger@syntro.ch>
2017-03-27 13:55:52 +0200

    added skeleton

--- spherpro/datastore.py
+++ spherpro/datastore.py
@@ -23,3 +23,57 @@ class DataStore(object):
     def readData(self):
         # Read the data based on the config
         raise NotImplementedError
+
+
+    ##########################################
+    #   Helper functions used by readData:   #
+    ##########################################
+
+    def readExperimentLayout(self, layoutfile):
+        # Read and validate the experiment layout
+        raise NotImplementedError
+
+    def readBarcodeKey(self, barcodefile):
+        # Read and validate the barcode key
+        raise NotImplementedError
+
+    def readWellMeasurements(self, wellmesfile):
+        # Read and validate the well measurements
+        raise NotImplementedError
+
+    def readChannelMeta(self, channelfile):
+        # Read and validate the channel metadata
+        raise NotImplementedError
+
+    def readChannelMeta(self, channelfile):
+        # Read and validate the channel metadata
+        raise NotImplementedError
+
+    def readRoiMeta(self, roifile):
+        # Read and validate the ROI metadata
+        raise NotImplementedError
+
+    def readChannelMeta(self, channelfile):
+        # Read and validate the channel metadata
+        raise NotImplementedError
+
+    def readMeasurementData(self):
+        # Read and validate the measurement data
+        raise NotImplementedError
+
+    def generateSphereMeta(self):
+        # Generate the sphere metadata
+        # 1. empty DF
+        # 2. debarcode and merge info (WellID, PlateID)
+        # 3. Merge meta info about spheres
+        #
+        raise NotImplementedError
+
+    ##########################################
+    #               Debarcoding:             #
+    ##########################################
+
+    def debarcode(self):
+        # Use data in self for debarcoding
+        raise NotImplementedError
+        # return a DF where Index is Spherenumber and rows are | Valid_BC_counts | highest_BC_counts | second_BC_count | well |
