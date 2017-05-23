96d7d02d703ff28e69a10245dda53bbbecd24e6a
Matthias Leutenegger <mleutenegger@syntro.ch>
2017-05-23 10:55:01 +0200

    update

--- spherpro/bro.py
+++ spherpro/bro.py
@@ -3,9 +3,6 @@ import numpy as np
 import re
 
 import spherpro as spp
-import spherpro.library as lib
-import spherpro.db as db
-import spherpro.datastore.DataStore as datastore
 
 class Bro(object):
     """docstring for Bro."""
@@ -15,6 +12,44 @@ class Bro(object):
 
     #########################################################################
     #########################################################################
+    #                         preparation functions:                        #
+    #########################################################################
+    #########################################################################
+
+    def calculate_dist_rim(self, assumed_spheresize = None):
+        """calculate_dist_rim
+        calculates the distance to rim and saves it to the DB.
+        By default, we calculate the distance using the real sphere radius
+        saved in the Image meta.
+        If you add the assumed_spheresize, then an equal real radius is assumed
+        for all spheres.
+        The distance_to_rim is calculated using the MeanIntensity of the DistStack.
+
+        Args:
+            assumed_spheresize:
+                - If None, then take sphere radius from Well images
+                - If Float, then assume this for all spheres
+                to be the real radius
+                - If 'estimate', the real radius is estimated by assuming
+                the largest sphere has the correct radius
+        """
+
+        if assumed_spheresize is None:
+            raise NotImplementedError('This feature is not available yet!')
+        elif (type(assumed_spheresize) is float) | (type(assumed_spheresize) is int):
+            print("assuming size you gave me!")
+        elif (type(assumed_spheresize) is str) & (assumed_spheresize == 'estimate'):
+            print("assuming size from all images!")
+        else:
+            raise NameError('Please specify a valid option!')
+
+
+
+    #########################################################################
+    #########################################################################
     #                         Quick plot functions:                         #
     #########################################################################
     #########################################################################
+
+    def draw_scatterplot(arg):
+        raise NotImplementedError
