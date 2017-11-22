206c0b55865f053967f7a3c3a61c4ece411e2088
votti <vito.zanotelli@gmail.com>
2017-11-22 15:07:23 +0000

    Adds options to the is-hq filter module (issue #99)

--- spherpro/bromodules/filter_stack_hq.py
+++ spherpro/bromodules/filter_stack_hq.py
@@ -22,47 +22,69 @@ class StackHQ(filter_base.BaseFilter):
         super().__init__(bro)
         self.custfilter = filter_customfilterstack.CustomFilterStack(bro)
 
-    def create(self, minfrac=0.01):
+        self.col_issphere_issphere = 'is-sphere'
+        self.col_isother_issphere = 'is-other'
+        self.col_measure_issphere = 'MeanIntensity'
+        self.col_stack_issphere = 'BinStack'
+        self.outcol_issphere = 'is-sphere-out'
+        self.non_zero_offset = 1/10000
+
+
+        self.col_distother = "dist-other"
+        self.col_measure = "MeanIntensity"
+        self.col_stack = "DistStack"
+        self.outcol_ambigous = "is-ambigous-out"
+
+    def create(self, minfrac=0.01, distother=-5,
+               filname=None, drop=False):
         """
         Creates the HQ filterdata and saves it to the db
+
+        Args:
+            minfrac (float): fraction of pixel that need to be in belong to the sphere in order to assign it
+            to the sphere.
+            distother (float): distance to the next neightbour (negative)
+            filname (str): name of the filter. Default: is-hq
+            drop (bool): overwrite existing filter with the same name? Default: False
         """
-        col_issphere_issphere = 'is-sphere'
-        col_isother_issphere = 'is-other'
-        col_measure_issphere = 'MeanIntensity'
-        col_stack_issphere = 'BinStack'
-        outcol_issphere = 'is-sphere-out'
-        non_zero_offset = 1/10000
+        if filname is None:
+            filname = 'is-hq'
+
+        col_issphere_issphere = self.col_issphere_issphere
+        col_isother_issphere = self.col_isother_issphere
+        col_measure_issphere = self.col_measure_issphere
+        col_stack_issphere = self.col_stack_issphere
+        outcol_issphere = self.outcol_issphere
+        non_zero_offset = self.non_zero_offset
 
         # Check Cells to be spherical
             # get MI issphere
             # get MI isother
-        issphere = self._get_joined_measurements([(col_stack_issphere,col_measure_issphere,col_issphere_issphere),(col_stack_issphere,col_measure_issphere,col_isother_issphere)])
+        issphere = self._get_joined_measurements([(col_stack_issphere,col_measure_issphere,col_issphere_issphere),
+                                                  (col_stack_issphere,col_measure_issphere,col_isother_issphere)])
         issphere[outcol_issphere] = pd.DataFrame(
-            (
-                (
-                    issphere[col_issphere_issphere]+non_zero_offset
-                )
-                /
-                (
-                    issphere[col_isother_issphere]+non_zero_offset
-                ) > minfrac
-            )
-        )
+            (((issphere[col_issphere_issphere]+non_zero_offset) >=
+             (issphere[col_isother_issphere]+non_zero_offset))) &
+            (issphere[col_issphere_issphere] >  minfrac))
+    
         # Check if cells are on rim touching another spheroid
-            # get MinI dist-other
-        col_distother = "dist-other"
-        col_measure = "MinIntensity"
-        col_stack = "DistStack"
-        outcol_ambigous = "is-ambigous-out"
+        # get MinI dist-other
+
+        col_distother = self.col_distother
+        col_measure = self.col_measure
+        col_stack = self.col_stack
+        outcol_ambigous = self.outcol_ambigous
+
         isambigous = self._get_joined_measurements([(col_stack,col_measure,col_distother)])
         isambigous[outcol_ambigous] = pd.DataFrame(
             (
-                isambigous[col_distother] > -5
+                isambigous[col_distother] > distother
             )
             &
             (
                 isambigous[col_distother] != (2**16)
             )
+            & (isambigous[col_distother
         )
         data = self.custfilter._get_valueless_table()
         data = data.set_index([db.KEY_IMAGENUMBER, db.KEY_OBJECTNUMBER])
@@ -72,7 +94,7 @@ class StackHQ(filter_base.BaseFilter):
         data[db.KEY_FILTERVALUE] = pd.DataFrame(
             data[outcol_issphere] & (data[outcol_ambigous] == False)
         )
-        self.custfilter.write_filter_to_db(data, "is-hq")
+        self.custfilter.write_filter_to_db(data, filname, drop=drop)
         return data
 
     def _get_joined_measurements(self, mdicts):
@@ -80,8 +102,18 @@ class StackHQ(filter_base.BaseFilter):
         gets the joined tables according to the names in list
         """
         sname, mname, cname = mdicts.pop(0)
-        q  = (self.data.get_measurement_query()
-                     .filter(
+        base_query = (self.session.query(
+                                db.Objects.ImageNumber,
+                                db.Objects.ObjectID,
+                                db.Objects.ObjectNumber,
+                                db.Measurement.Value,
+                                db.RefStack.Scale
+                               )
+                    .join(db.Measurement)
+                    .join(db.PlaneMeta)
+                    .join(db.RefPlaneMeta)
+                    .join(db.RefStack))
+        q  = (base_query.filter(
                          self.bro.filters.measurements.get_measurement_filter_statements(
                              channel_names=[cname],
                              object_ids=['cell'],
@@ -92,11 +124,11 @@ class StackHQ(filter_base.BaseFilter):
                     )
         data = pd.read_sql_query(q.statement,self.data.db_conn)
         data = data.set_index([db.KEY_IMAGENUMBER, db.KEY_OBJECTNUMBER])
-        data[cname] = data[db.KEY_VALUE]
+        data[cname] = data[db.KEY_VALUE] * data[db.KEY_SCALE]
         data = pd.DataFrame(data[cname])
         for group in mdicts:
             sname, mname, cname = group
-            q  = (self.data.get_measurement_query()
+            q  = (base_query
                          .filter(
                              self.bro.filters.measurements.get_measurement_filter_statements(
                                  channel_names=[cname],
@@ -108,7 +140,7 @@ class StackHQ(filter_base.BaseFilter):
                         )
             tmp = pd.read_sql_query(q.statement,self.data.db_conn)
             tmp = tmp.set_index([db.KEY_IMAGENUMBER, db.KEY_OBJECTNUMBER])
-            tmp[cname] = tmp[db.KEY_VALUE]
+            tmp[cname] = tmp[db.KEY_VALUE] * tmp[db.KEY_SCALE]
             tmp = pd.DataFrame(tmp[cname])
             data = data.join(tmp)
         return data
