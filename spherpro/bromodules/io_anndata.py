62aedc9253437cea5b1d23d3cc7d4827caa03833
votti <vito.zanotelli@gmail.com>
2019-12-11 10:18:31 +0000

    Adds IoObjMeasurements
    This allows to query measurements/objects via anndata.

--- spherpro/bromodules/io_anndata.py
+++ spherpro/bromodules/io_anndata.py
@@ -21,7 +21,6 @@ def get_anndata_filename(conf: object, object_type: str):
     fn = pathlib.Path(conf['sqlite']['db']).parent / (object_type + SUFFIX_ANNDATA)
     return fn
 
-
 class IoAnnData(io_base.BaseIo):
     def __init__(self, bro, obj_type):
         super().__init__(bro)
@@ -35,8 +34,8 @@ class IoAnnData(io_base.BaseIo):
         """
         return get_anndata_filename(self.bro.data.conf, self.obj_type)
 
-    def initialize_anndata(self, data):
-        adat = ad.AnnData(data)
+    def initialize_anndata(self, data, var=None, obs=None):
+        adat = ad.AnnData(data, var=var, obs=obs)
         adat.write(self.filename)
 
     @property
@@ -46,6 +45,29 @@ class IoAnnData(io_base.BaseIo):
                                          filemode='r')
         return self._adat
 
+class IoObjMeasurements:
+    def __init__(self, bro):
+        self.bro = bro
+        self.anndatdict = dict()
 
+    def get_anndat(self, obj_type):
+        anndat = self.anndatdict.get(obj_type, None)
+        if anndat is None:
+            anndat = IoAnnData(bro, obj_type)
+            self.anndatdict[obj_type] = anndat
+        return anndat.adat
 
-
+    def get_measurements(self, dat_obj, dat_meas):
+        measids = set(map(str, dat_meas[db.measurements.measurement_id.key]))
+        dats = []
+        for objtype, grp in dat_obj.groupby(db.objects.object_type.key):
+            adat = self.get_anndat(objtype)
+            obsidx = list(map(str, grp[db.objects.object_id.key]))
+            varidx = list(measids.intersection(adat.var.index))
+            if (len(varidx) > 0) & (len(obsidx) > 0):
+                dats.append(anndata.AnnData(adat[obsidx, :].X, obs=adat.obs.loc[obsidx,:], var=adat.var)[:,varidx].copy())
+                print(dats[-1].shape)
+        if len(dats) == 1:
+            return dats[0]
+        else:
+            return anndata.AnnData.concatenate(*dats, join='outer',index_unique=None)
