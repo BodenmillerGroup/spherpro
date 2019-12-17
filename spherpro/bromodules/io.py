e1b89e31665edaa47c519970cc74e846fe6f3c19
votti <vito.zanotelli@gmail.com>
2019-12-17 09:17:56 +0000

    Add io.objmeasurements

--- spherpro/bromodules/io.py
+++ spherpro/bromodules/io.py
@@ -1,6 +1,10 @@
 import spherpro.bromodules.io_masks as io_masks
 import spherpro.bromodules.io_imcfolder as io_imc
+import spherpro.bromodules.io_anndata as io_ann
+
+
 class Io(object):
     def __init__(self, bro):
         self.masks = io_masks.IoMasks(bro)
         self.imcimg = io_imc.IoImc(bro)
+        self.objmeasurements = io_ann.IoObjMeasurements(bro)
