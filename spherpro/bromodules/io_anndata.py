5f958ab6ab6e979522d4006b0f3e571c3d613703
votti <vito.zanotelli@gmail.com>
2019-12-17 09:04:07 +0000

    Fix severe bug in anndata reading
    This has caused a mismatch between var and X

--- spherpro/bromodules/io_anndata.py
+++ spherpro/bromodules/io_anndata.py
@@ -54,26 +54,50 @@ class IoAnnData(io_base.BaseIo):
 class IoObjMeasurements:
     def __init__(self, bro):
         self.bro = bro
-        self.anndatdict = dict()
+        self._anndatdict = dict()
+        self.scale_anndata = scale_anndata
 
     def get_anndat(self, obj_type):
-        anndat = self.anndatdict.get(obj_type, None)
+        anndat = self._anndatdict.get(obj_type, None)
         if anndat is None:
-            anndat = IoAnnData(bro, obj_type)
-            self.anndatdict[obj_type] = anndat
+            anndat = IoAnnData(self.bro, obj_type)
+            self._anndatdict[obj_type] = anndat
         return anndat.adat
 
-    def get_measurements(self, dat_obj, dat_meas):
-        measids = set(map(str, dat_meas[db.measurements.measurement_id.key]))
-        dats = []
-        for objtype, grp in dat_obj.groupby(db.objects.object_type.key):
-            adat = self.get_anndat(objtype)
-            obsidx = list(map(str, grp[db.objects.object_id.key]))
-            varidx = list(measids.intersection(adat.var.index))
-            if (len(varidx) > 0) & (len(obsidx) > 0):
-                dats.append(anndata.AnnData(adat[obsidx, :].X, obs=adat.obs.loc[obsidx,:], var=adat.var)[:,varidx].copy())
-                print(dats[-1].shape)
-        if len(dats) == 1:
-            return dats[0]
+    def get_measurements(self, dat_obj=None, dat_meas=None, *, measidx=None, objidx=None, object_type=None):
+        if dat_meas is not None:
+            dat_meas = dat_meas.sort_values(db.measurements.measurement_id.key)
+            measids = list(map(str, dat_meas[db.measurements.measurement_id.key]))
+            dat_meas.index = measids
         else:
-            return anndata.AnnData.concatenate(*dats, join='outer',index_unique=None)
+            measids = set(map(str, sorted(measidx)))
+
+        if dat_obj is not None:
+            dat_obj = dat_obj.sort_values(db.objects.object_id.key)
+            obsidx = list(map(str, dat_obj[db.objects.object_id.key]))
+            dat_obj.index = obsidx
+            it = dat_obj.groupby(db.objects.object_type.key)
+        else:
+            class obs:
+                index = list(map(str, sorted(objidx)))
+            it = [(object_type, obs)]
+
+        dats = []
+        for objtype, grp in it:
+            obsidx = grp.index
+            adat = self.get_anndat(objtype)
+            varidx = list(set(measids).intersection(adat.var.index))
+            if (len(varidx) > 0) & (len(obsidx) > 0):
+                adat = adat[:, sorted(varidx)]
+                dats.append(ad.AnnData(adat.X, obs=adat.obs, var=adat.var)[obsidx,:].copy())
+        if len(dats) == 1:
+            dat = dats[0]
+        elif len(dats) == 0:
+            raise ValueError('No valid measurements found')
+        else:
+            dat = ad.AnnData.concatenate(*dats, join='outer',index_unique=None)
+        if dat_meas is not None:
+            dat.var = dat.var.join(dat_meas)
+        if dat_obj is not None:
+            dat.obs = dat.obs.join(dat_obj)
+        return dat
