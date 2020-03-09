b3760131a648e09f338d5c3563371cec05ff00c5
votti <mcvotti@gmail.com>
2020-03-09 11:16:09 +0100

    add anndata helpers to helpers

--- spherpro/bromodules/helpers.py
+++ spherpro/bromodules/helpers.py
@@ -1,5 +1,7 @@
 import spherpro.bromodules.helpers_varia as hv
+import spherpro.bromodules.helpers_anndata as ha
 
 class Helpers(object):
     def __init__(self, bro):
         self.dbhelp = hv.HelperDb(bro)
+        self.anndata = ha
