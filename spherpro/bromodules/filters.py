c59626982cffa1f5f41f5bfce719cf8c355f9a26
votti <mcvotti@gmail.com>
2020-04-23 15:40:39 +0200

    Deleted legacy hq filter

--- spherpro/bromodules/filters.py
+++ spherpro/bromodules/filters.py
@@ -1,7 +1,6 @@
 import spherpro.bromodules.filter_measurements as filter_measurements
 import spherpro.bromodules.filter_membership as filter_membership
 import spherpro.bromodules.filter_objectfilters as filter_objectfilters
-import spherpro.bromodules.filter_stack_hq as filter_stack_hq
 
 
 class Filters(object):
@@ -9,4 +8,3 @@ class Filters(object):
         self.membership = filter_membership.FilterMembership(bro)
         self.measurements = filter_measurements.FilterMeasurements(bro)
         self.objectfilterlib = filter_objectfilters.ObjectFilterLib(bro)
-        self.stack_hq = filter_stack_hq.StackHQ(bro)
